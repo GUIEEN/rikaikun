c3ae8414081520676eaa100afdf829d1a7aa896c
Erek Speed <melink14@gmail.com>
2019-09-15 10:52:46 -0700

    Add eslint config and fix background.js so it has no errors.

--- background.js
+++ background.js
@@ -2,8 +2,8 @@
 chrome.browserAction.onClicked.addListener(rcxMain.inlineToggle);
 chrome.tabs.onSelectionChanged.addListener(rcxMain.onTabSelect);
 chrome.runtime.onMessage.addListener(
-	function(request, sender, response) {
-		switch(request.type) {
+	function (request, sender, response) {
+		switch (request.type) {
 			case 'enable?':
 				console.log('enable?');
 				rcxMain.onTabSelect(sender.tab.id);
@@ -13,18 +13,18 @@ chrome.runtime.onMessage.addListener(
 				var e = rcxMain.search(request.text, request.dictOption);
 				response(e);
 				break;
-/*			case 'nextDict':
+				/*			case 'nextDict':
 				console.log('nextDict');
 				rcxMain.nextDict();
-				break;*/
+				break; */
 			case 'resetDict':
 				console.log('resetDict');
 				rcxMain.resetDict();
 				break;
 			case 'translate':
 				console.log('translate');
-				var e = rcxMain.dict.translate(request.title);
-				response(e);
+				var translate = rcxMain.dict.translate(request.title);
+				response(translate);
 				break;
 			case 'makehtml':
 				console.log('makehtml');
@@ -33,11 +33,12 @@ chrome.runtime.onMessage.addListener(
 				break;
 			case 'switchOnlyReading':
 				console.log('switchOnlyReading');
-				if(rcxMain.config.onlyreading == 'true')
+				if (rcxMain.config.onlyreading === 'true') {
 					rcxMain.config.onlyreading = 'false';
-				else
+				} else {
 					rcxMain.config.onlyreading = 'true';
-				localStorage['onlyreading'] = rcxMain.config.onlyreading;
+				}
+				localStorage.onlyreading = rcxMain.config.onlyreading;
 				break;
 			case 'copyToClip':
 				console.log('copyToClip');
@@ -47,82 +48,75 @@ chrome.runtime.onMessage.addListener(
 				console.log(request);
 		}
 	});
-	
-if(initStorage("v0.8.92", true)) {
+
+if (initStorage('v0.8.92', true)) {
 	// v0.7
-	initStorage("popupcolor", "blue");
-	initStorage("highlight", true);
-	
+	initStorage('popupcolor', 'blue');
+	initStorage('highlight', true);
+
 	// v0.8
 	// No changes to options
-	
+
 	// V0.8.5
-	initStorage("textboxhl", false);
+	initStorage('textboxhl', false);
 
 	// v0.8.6
-	initStorage("onlyreading", false);
+	initStorage('onlyreading', false);
 	// v0.8.8
-	if (localStorage['highlight'] == "yes")
-		localStorage['highlight'] = "true";
-	if (localStorage['highlight'] == "no")
-		localStorage['highlight'] = "false";
-	if (localStorage['textboxhl'] == "yes")
-		localStorage['textboxhl'] = "true";
-	if (localStorage['textboxhl'] == "no")
-		localStorage['textboxhl'] = "false";
-	if (localStorage['onlyreading'] == "yes")
-		localStorage['onlyreading'] = "true";
-	if (localStorage['onlyreading'] == "no")
-		localStorage['onlyreading'] = "false";
-	initStorage("copySeparator", "tab");
-	initStorage("maxClipCopyEntries", "7");
-	initStorage("lineEnding", "n");
-	initStorage("minihelp", "true");
-	initStorage("disablekeys", "false");
-	initStorage("kanjicomponents", "true");
+	if (localStorage.highlight === 'yes') { localStorage.highlight = 'true'; }
+	if (localStorage.highlight === 'no') { localStorage.highlight = 'false'; }
+	if (localStorage.textboxhl === 'yes') { localStorage.textboxhl = 'true'; }
+	if (localStorage.textboxhl === 'no') { localStorage.textboxhl = 'false'; }
+	if (localStorage.onlyreading === 'yes') { localStorage.onlyreading = 'true'; }
+	if (localStorage.onlyreading === 'no') { localStorage.onlyreading = 'false'; }
+	initStorage('copySeparator', 'tab');
+	initStorage('maxClipCopyEntries', '7');
+	initStorage('lineEnding', 'n');
+	initStorage('minihelp', 'true');
+	initStorage('disablekeys', 'false');
+	initStorage('kanjicomponents', 'true');
 
-	for (i = 0; i*2 < rcxDict.prototype.numList.length; i++) {
-		initStorage(rcxDict.prototype.numList[i*2], "true")
+	for (var i = 0; i * 2 < rcxDict.prototype.numList.length; i++) {
+		initStorage(rcxDict.prototype.numList[i * 2], 'true');
 	}
 
-    // v0.8.92
-	initStorage("popupDelay", "150");
-	initStorage("showOnKey", "");
+	// v0.8.92
+	initStorage('popupDelay', '150');
+	initStorage('showOnKey', '');
 }
 
-/** 
-* Initializes the localStorage for the given key. 
-* If the given key is already initialized, nothing happens. 
-* 
+/**
+* Initializes the localStorage for the given key.
+* If the given key is already initialized, nothing happens.
+*
 * @author Teo (GD API Guru)
-* @param key The key for which to initialize 
-* @param initialValue Initial value of localStorage on the given key 
-* @return true if a value is assigned or false if nothing happens 
-*/ 
-function initStorage(key, initialValue) {
-  var currentValue = localStorage[key];
-  if (!currentValue) {
-	localStorage[key] = initialValue;
-	return true;
-  }
-  return false;
+* @param key The key for which to initialize
+* @param initialValue Initial value of localStorage on the given key
+* @return true if a value is assigned or false if nothing happens
+*/
+function initStorage (key, initialValue) {
+	var currentValue = localStorage[key];
+	if (!currentValue) {
+		localStorage[key] = initialValue;
+		return true;
+	}
+	return false;
 }
 
 rcxMain.config = {};
-rcxMain.config.css = localStorage["popupcolor"];
-rcxMain.config.highlight = localStorage["highlight"];
-rcxMain.config.textboxhl = localStorage["textboxhl"];
-rcxMain.config.onlyreading = localStorage["onlyreading"];
-rcxMain.config.copySeparator = localStorage["copySeparator"];
-rcxMain.config.maxClipCopyEntries = localStorage["maxClipCopyEntries"];
-rcxMain.config.lineEnding = localStorage["lineEnding"];
-rcxMain.config.minihelp = localStorage["minihelp"];
-rcxMain.config.popupDelay = parseInt(localStorage["popupDelay"]);
-rcxMain.config.disablekeys = localStorage["disablekeys"];
-rcxMain.config.showOnKey = localStorage["showOnKey"];
-rcxMain.config.kanjicomponents = localStorage["kanjicomponents"];
-rcxMain.config.kanjiinfo = new Array(rcxDict.prototype.numList.length/2);
-for (i = 0; i*2 < rcxDict.prototype.numList.length; i++) {
-	rcxMain.config.kanjiinfo[i] = localStorage[rcxDict.prototype.numList[i*2]];
+rcxMain.config.css = localStorage.popupcolor;
+rcxMain.config.highlight = localStorage.highlight;
+rcxMain.config.textboxhl = localStorage.textboxhl;
+rcxMain.config.onlyreading = localStorage.onlyreading;
+rcxMain.config.copySeparator = localStorage.copySeparator;
+rcxMain.config.maxClipCopyEntries = localStorage.maxClipCopyEntries;
+rcxMain.config.lineEnding = localStorage.lineEnding;
+rcxMain.config.minihelp = localStorage.minihelp;
+rcxMain.config.popupDelay = parseInt(localStorage.popupDelay);
+rcxMain.config.disablekeys = localStorage.disablekeys;
+rcxMain.config.showOnKey = localStorage.showOnKey;
+rcxMain.config.kanjicomponents = localStorage.kanjicomponents;
+rcxMain.config.kanjiinfo = new Array(rcxDict.prototype.numList.length / 2);
+for (i = 0; i * 2 < rcxDict.prototype.numList.length; i++) {
+	rcxMain.config.kanjiinfo[i] = localStorage[rcxDict.prototype.numList[i * 2]];
 }
-
